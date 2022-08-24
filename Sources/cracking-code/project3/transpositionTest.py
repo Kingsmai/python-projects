fce2abdb7452165c6af16755472f7c5da17f2c7a
Xiaomai <xsbugh@gmail.com>
2022-08-24 11:11:17 +0800

    added transposition auto test

---
+++ Sources/cracking-code/project3/transpositionTest.py
@@ -0,0 +1,36 @@
+import sys, random, transpositionEncrypt, transpositionDecrypt
+
+def main():
+    random.seed(42) # Set the random "seed" to a static value
+
+    for i in range(20): # Run 20 tests
+        # Generate random messages to test
+
+        # The message will have a random length;
+        message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" * random.randint(4, 40)
+
+        # Convert the message string to a list to shuffle it:
+        message = list(message)
+        random.shuffle(message)
+        message = "".join(message) # Convert the list back to string
+
+        print("Test #%s: %s..." % (i + 1, message[:50]))
+
+        # Check all possible keys for each message:
+        for key in range(1, int(len(message) / 2)):
+            encrypted = transpositionEncrypt.encryptMessage(key, message)
+            decrypted = transpositionDecrypt.decryptMessage(key, encrypted)
+
+            # If the decryption doesn't match the original message, display
+            # an error message and quit:
+            if (message != decrypted):
+                print('Mismatch with key %s and message %s.' % (key, message))
+                print('Decrypted as: %s' % (decrypted))
+                sys.exit()
+
+    print("Transposition cipher test passed!")
+
+
+if __name__ == "__main__":
+    main()
+                
