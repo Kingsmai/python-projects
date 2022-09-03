27c894f26fcb898200354c18cfcd8728bf031267
Xiaomai <xsbugh@gmail.com>
2022-09-03 14:59:08 +0800

    Update Code

--- Sources/NFT_forceRefresh.py
+++ Sources/NFT_forceRefresh.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
 SUCCESS_MSG = "We've queued this item for an update! Check back in a minute..."
 
 start_time = time.time()
+
+failed_no = []
 
 driver = webdriver.Firefox()
 for i in range(0, 8888):
@@ -15,12 +16,19 @@ for i in range(0, 8888):
         driver.get(url)
         btn = driver.find_element(By.CSS_SELECTOR, "button.sc-1xf18x6-0.sc-glfma3-0.hiIVBZ.gVeaIW.sc-1skvztv-0.fPnOUC:nth-child(1)")
         btn.click()
+        time.sleep(0.2)
+        msg = driver.find_element(By.CSS_SELECTOR, "#__next > div.sc-1xf18x6-0.sc-1twd32i-0.sc-ihijgw-0.haVRLx.kKpYwv.gpOOSX > div > div.sc-1xf18x6-0.sc-1twd32i-0.iQOhGx.kKpYwv")
+        if msg.text != SUCCESS_MSG:
+            failed_no.append(i)
     except:
-        continue
+        failed_no.append(i)
     finally:
         time.sleep(1)
-# driver.close()
+driver.close()
 
 end_time = time.time()
 
+success = True
+
 print(f'Operation {"SUCCESS" if success else "FAILED"} in {end_time - start_time}')
+print(f'FAILED NFTs => {failed_no}')
