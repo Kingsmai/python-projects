ce77a1437de96d1d2e8f57c3ab5474b4488c2f53
Xiaomai <xsbugh@gmail.com>
2022-09-09 14:39:45 +0800

    research unicode (more character)

---
+++ Sources/unicode-trick.py
@@ -0,0 +1,27 @@
+lastchar = 120484
+firstchar = lastchar - (26 * 26)
+upper = 65
+lower = 97
+
+with open('Data/Unicode.txt', 'w', encoding='utf-8') as file:
+    for idx, i in enumerate(range(upper, upper + (26))):
+        # currentChar = f'{chr(i)}, {i}\n'
+        currentChar = f'{chr(i)}'
+        file.write(currentChar + ' ')
+        if (idx + 1) % 26 == 0:
+            file.write('\n')
+
+    for idx, i in enumerate(range(lower, lower + (26))):
+        # currentChar = f'{chr(i)}, {i}\n'
+        currentChar = f'{chr(i)}'
+        file.write(currentChar + ' ')
+        if (idx + 1) % 26 == 0:
+            file.write('\n')
+
+    for idx, i in enumerate(range(firstchar, lastchar)):
+        # currentChar = f'{chr(i)}, {i}\n'
+        currentChar = f'{chr(i)}'
+        file.write(currentChar + ' ')
+        if (idx + 1) % 26 == 0:
+            file.write('\n')
+
